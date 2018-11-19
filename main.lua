efc5b7194284d41d40823430be089c13efb925bc
yazgoo <yazgoo@gmail.com>
2018-11-20 00:34:18 +0100

    add plug_update

--- main.lua
+++ main.lua
@@ -21,13 +21,23 @@ end
 function basename(path)
   return string.match(path, ".*/([^/]+)$")
 end
+plugins = {"git@github.com:yazgoo/elinks-plug"}
+function plug_directory(url)
+  return cwd() .. "/../" .. basename(url)
+end
 function plug(url)
-  local path = basename(url)
-  local directory = cwd() .. "/../" .. path
+  table.insert(plugins, url)
+  local directory = plug_directory(url)
   if not exists(directory) then
     print("cloning")
     print("git clone " .. url .. " " .. directory)
     os.execute("git clone " .. url .. " " .. directory)
   end
   source(directory .. "/main.lua")
+end
+function plug_update()
+  os.execute("clear")
+  for _, url in pairs(plugins) do
+    os.execute("git -C " ..  plug_directory(url) .. " pull origin master")
+  end
 end
