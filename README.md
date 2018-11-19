3c46d61eba9682693ea7704caf5e681eef1bb4d3
yazgoo <yazgoo@gmail.com>
2018-11-19 21:28:14 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# elinks-plug
+# installing
+
+```
+$ mkdir -p ~/.elinks/
+$ cd ~/.elinks
+$ git clone http://github.com/yazgoo/elinks-plug
+
+in ~/.elinks/, Add this line at the top of hooks.lua
+
+```lua
+loadfile(debug.getinfo(1, "S").source:sub(2):match("(.*/)") .. "/elinks-plug/plug.lua")()
+```
+
+# adding plugins
+
+in your hooks.lua, add plugins, e.g.
+
+```
+plug("http://github.com/yazgoo/elinks-basis")
+```
+
+then start elinks to install them.
---
+++ plug.lua
@@ -0,0 +1,28 @@
+function script_path() return debug.getinfo(2, "S").source:sub(2) end
+function cwd()
+  return script_path():match("(.*/)")
+end
+function load_script(script)
+  assert(loadfile(cwd() .. "/" .. script))()
+end
+function exists(file)
+   local ok, err, code = os.rename(file, file)
+   if not ok then
+      if code == 13 then
+         return true
+      end
+   end
+   return ok, err
+end
+function basename(path)
+  return string.match(path, ".*/([^/]+)$")
+end
+function plug(url)
+  local path = basename(url)
+  local directory = cwd() .. "/../" .. path
+  if not exists(directory) then
+    print("cloning")
+    print("git clone " .. url .. " " .. directory)
+    os.execute("git clone " .. url .. " " .. directory)
+  end
+end
